8bfe1ff786aae9863a61451405661cf0b7b116e4
Piotr Dubiel <piotr.dubiel@polidea.com>
2020-10-25 18:47:23 +0100

    Initialize new example

---
+++ packages/ReasonableLottie/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+
+const APP_NAME = "ReasonableLottie";
+
+AppRegistry.registerComponent(APP_NAME, () => App);
